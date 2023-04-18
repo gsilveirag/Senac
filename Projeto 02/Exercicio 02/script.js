2cab5d3efe7d8d24ca89e6947033436909b91e95
GUSTAVO GONCALVES <gustavo.silveira.goncalves@gmail.com>
2023-04-18 18:00:53 -0300

    Versao 4.0

--- Projeto 02/Exercicio 02/script.js
+++ Projeto 02/Exercicio 02/script.js
@@ -3,6 +3,7 @@ var array = []
 
 class Produto {
     constructor (){
+        this.id = 1
         this.nome = document.getElementById('nome').value
         this.codigo = document.getElementById('codigo').value
     }
@@ -10,6 +11,7 @@ class Produto {
     adicionar(){
         
         let novoItem = {
+            id : this.id,
             nome : nome.value,
             codigo : codigo.value
         }
@@ -24,71 +26,91 @@ class Produto {
             if(localStorage.meuArray){ // verifica se ja existe
                 array = JSON.parse(localStorage.getItem('meuArray')) // ele puxa e converte
             }
-    
             array.push(novoItem) // adiciona as info no array
-    
-            document.getElementById('nome').value = '' // limpa os dados
-            document.getElementById('codigo').value = '' // limpa os dados
-    
+
+            this.id++
             localStorage.meuArray = JSON.stringify(array) // converte em String e armazena no meuArray)
 
+            this.recuperar() // chama o metodo para mostrar os elementos
+            this.limpar() // limpa a tela apos enviar
         }   
     }
 
     recuperar(){
         let dados = document.getElementById('dados')
-        dados.innerHTML = '' // limpa a div para mostrar os dados
+        dados.innerText = ''
 
         if(localStorage.meuArray){
             array = JSON.parse(localStorage.getItem('meuArray'))
         }
         
         for (let i = 0; i < array.length; i++){ // funcao para imprimir na tela tudo salvo
-            let trNome = document.createElement('li')
-            trNome.innerText = "Nome: " +array[i].nome + " Codigo: " +array[i].codigo
-            dados.appendChild(trNome)
+            let tr = dados.insertRow()
+
+            let tdId = tr.insertCell()
+            let tdNome = tr.insertCell()
+            let tdCodigo = tr.insertCell()
+            let tdAcao = tr.insertCell()
             
+            tdId.innerText = array[i].id
+            tdNome.innerText = array[i].nome
+            tdCodigo.innerText = array[i].codigo
+            
+            let iconEdit = document.createElement('img')
+            iconEdit.src = 'img/edit_icon.png'
+            iconEdit.setAttribute("onclick", "produto.editar(" + array[i].id  +")")
+
+            let iconDelete = document.createElement('img')
+            iconDelete.src = 'img/delete_icon.png'
+            iconDelete.setAttribute("onclick", "produto.apagar("+  array[i].id +")")
+
+
+            tdAcao.appendChild(iconEdit)
+            tdAcao.appendChild(iconDelete)
         }
     }
 
     limpar(){
-        array = [] // zera o array
-        localStorage.meuArray = JSON.stringify(array) // envia o array zerado
-        dados.innerHTML = '' // limpa a tela
+        document.getElementById('nome').value = '' // limpa os dados
+        document.getElementById('codigo').value = '' // limpa os dados
     }
 
     remove(){
-        dados.innerHTML = ''
-    if(localStorage.meuArray){
-        array = JSON.parse(localStorage.getItem('meuArray'))
-    }
-    
-        const map = new Map()
 
-        array.forEach(element => { // percorre todo o array e elimina o nome duplicado
-            map.set(element.nome, element)
-        });
-
-        
-        const listaUnica = Array.from(map.values()) // envia para o novo array sem nada duplicado
-        array = [] //zera o antigo array
-    
-        for(let i = 0; i < listaUnica.length; i++){ //puxa os dados da nova lista e manda separado para a antiga
-            array.push(listaUnica[i])
-            localStorage.meuArray = JSON.stringify(array)
+        if(localStorage.meuArray){ // verifica se ja existe
+            array = JSON.parse(localStorage.getItem('meuArray')) // ele puxa e converte
         }
+        array = []
+        this.id = 1
+        array.push() // adiciona as info no array
+        
+        localStorage.meuArray = JSON.stringify(array) // converte em String e armazena no meuArray)
+        dados.innerText = ''
     }    
     
 
-    apagar(){
-        dados.innerHTML = ''
+    apagar(id){
+        let dados = document.getElementById('dados')
+
+        for(let i= 0; i < array.length; i++){
+            if(array[i].id === id){
+                array.splice(i, 1)
+                dados.deleteRow(i)
+            }
+        }
+    }
+
+    editar(id){
         if(localStorage.meuArray){
             array = JSON.parse(localStorage.getItem('meuArray'))
         }
-
-        array.pop()
-        array.push()
-        localStorage.meuArray = JSON.stringify(array)
+        
+        for(let i= 0; i < array.length; i++){
+            if(array[i].id === id){
+                document.getElementById('nome').value = array[i].nome
+                document.getElementById('codigo').value = array[i].codigo
+        }
+        }
     }
 }
 
