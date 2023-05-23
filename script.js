47f5272670bae3e8c3bb2e3994f8ec06c427f675
Sala 18A <sala18a@ceines.com.br>
2023-05-23 20:36:43 -0300

    Primeiro Versao

---
+++ script.js
@@ -0,0 +1,9 @@
+function mudarCorDeFundo(corFundo, corTexto, corBorda) {
+  var container = document.getElementById('container');
+  var quadradoUm = document.getElementById('quadradoUm');
+  var quadradoDois = document.getElementById('quadradoDois');
+  container.style.backgroundColor = corFundo;
+  container.style.color = corTexto;
+  quadradoUm.style.borderColor = corBorda;
+  quadradoDois.style.borderColor = corBorda;
+  }
